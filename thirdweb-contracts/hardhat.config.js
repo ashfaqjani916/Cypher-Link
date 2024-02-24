d176d92007ddfd1bd509e9811367dfee72c52487
Nikhil Pulluri <nikhilpulluri7810@gmail.com>
2024-02-25 00:28:51 +0530

    solidity version corrected

--- thirdweb-contracts/hardhat.config.js
+++ thirdweb-contracts/hardhat.config.js
@@ -32,7 +32,7 @@ module.exports = {
   //   tests: "./test",
   // },
   solidity: {
-    version: "0.8.17",
+    version: "0.8.9",
     defaultNetwork: "sepolia",
     networks: {
       hardhat:{},
