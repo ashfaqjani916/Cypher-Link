ea6ed55b6e6f92e93f926ea199c253fb5afeb5d4
lokeshdarla <lokeshnagasaidarla@gmail.com>
2024-02-24 15:06:44 +0530

    [feat] shadcn dependencies

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,10 @@
-
 import './App.css'
+import Home from '@/pages/Home'
 
 function App() {
-  
-
   return (
     <>
-      <div className="h-10 w-10 bg-black" > box</div>
+      <Home />
     </>
   )
 }
