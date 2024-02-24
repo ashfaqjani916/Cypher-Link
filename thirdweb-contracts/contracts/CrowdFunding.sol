f40453409f8fe75c5918077d0df6bfc8ad37910a
asfaqjani916 <ashfaqjani916@gmail.com>
2024-02-24 18:57:55 +0530

    [feat]added smart contract

--- thirdweb-contracts/contracts/CrowdFunding.sol
+++ thirdweb-contracts/contracts/CrowdFunding.sol
@@ -1,51 +1,69 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-// SPDX-License-Identifier: UNLICENSED
-
-
-contract CrowdFunding{
-    struct Campaign{
+contract CrowdFunding {
+    struct Campaign {
         address owner;
         string title;
         string description;
         uint256 target;
         uint256 deadline;
         uint256 amountCollected;
-        string img;
+        string image;
         address[] donators;
         uint256[] donations;
     }
 
+    mapping(uint256 => Campaign) public campaigns;
 
-    mapping(uint256 => Campaign) public campaings;
+    uint256 public numberOfCampaigns = 0;
 
-    uint256 public numberOfCampaings = 0;
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
 
-    function createCampaing(address _owner,string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _img) public returns (uint256) {
-        Campaign storage campaing = campaings[numberOfCampaings];
+        require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
 
-        //is everything okey?
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
 
-        require(campaing.deadline <block.timestamp, " The deadline should be a date in the future");     
+        numberOfCampaigns++;
 
-        campaing.owner = _owner;
-        campaing.title = _title;
-        campaing.description = _description;
-        campaing.target = _target;
-        campaing.deadline = _deadline;
-        campaing.amountCollected = 0;
-        campaing.img = _img;
-
-        numberOfCampaings++;
-         
-        return numberOfCampaings -1;
-         
+        return numberOfCampaigns - 1;
     }
 
-    function donateToCampaign() {}
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
 
-    function getDonators() {}
+        Campaign storage campaign = campaigns[_id];
 
-    function getCampaigns() {}
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
 
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+
+        if(sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
+
+    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
+
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+
+        for(uint i = 0; i < numberOfCampaigns; i++) {
+            Campaign storage item = campaigns[i];
+
+            allCampaigns[i] = item;
+        }
+
+        return allCampaigns;
+    }
 }
